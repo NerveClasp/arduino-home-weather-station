30bca7b35a952727b9babd80f8e8c1ddcce666eb
NerveClasp <nerveclasp@gmail.com>
2016-11-09 17:02:45 +0200

    Sample DHT11 temperature and humidity sensor code from arduino-ua.com

---
+++ arduino-home-weather-station.ino
@@ -0,0 +1,33 @@
+#include "DHT.h"
+#define DHTPIN 2    // к какому пину будет подключен вывод Data
+
+//выбор используемого датчика
+#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+//инициализация датчика
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  dht.begin();
+}
+
+void loop() {
+  // чтение температуры и влажности займет примерно 250 миллисекунд
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // проверяем правильные ли данные получили
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Error reading from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
